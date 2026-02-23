5b614176f78ce5e1635b610635023817f4eeffd3
tekihl <tom.kihlborg@gmail.com>
2026-02-23 19:19:43 +0100

    Created about page

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function aboutPage() {
+
+    return (
+        <>
+            <div>
+                <p>
+                    Nina makes these shirts
+                </p>
+            </div>
+        </>
+    )
+}
